106f575eb97233d1062a75897f65c8cc0e177ba5
amritsrivastava <srivastava.amrit1999@gmail.com>
2020-01-06 22:11:19 +0530

    boilerplate setup

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  secretKey: '12345-67890-09876-54321',
+  mongoUrl: 'mongodb://localhost:27017/conFusion',
+  facebook: {
+    clientId: '1234567890',
+    clientSecret: '12345-67890-09876-54321'
+  }
+};
