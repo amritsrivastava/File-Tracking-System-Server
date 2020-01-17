1e7bcef127a539c473d51936d9ebb7ec9cce4f8d
amritsrivastava <srivastava.amrit1999@gmail.com>
2020-01-17 15:54:13 +0530

    fix: QR path

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
   mongoUrl: 'mongodb://localhost:27017/fileTrackingSystem',
-  secureHost: 'https://localhost:3443/',
-  host: 'http://localhost:3000/',
+  secureHost: 'https://13.233.200.7:3443/',
+  host: 'http://13.233.200.7:3000/',
   facebook: {
     clientId: '1234567890',
     clientSecret: '12345-67890-09876-54321'
